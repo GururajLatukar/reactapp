10b70ea23609d1f098a16597a9cc8abe1eff56ec
GururajLatukar <gururaj.latukar@gmail.com>
2024-07-24 14:04:02 +0530

    add redux provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import { Provider } from 'react-redux';
+import store from './store';
+
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </Provider>
   );
 };
 
